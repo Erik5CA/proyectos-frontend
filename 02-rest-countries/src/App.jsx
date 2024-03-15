6133793930d9ef6906acfd943bf85966a018dcf3
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-14 18:18:02 -0600

    app

--- 02-rest-countries/src/App.jsx
+++ 02-rest-countries/src/App.jsx
@@ -1,9 +1,34 @@
 import "./App.css";
+import BarSup from "./components/BarSup";
+import Card from "./components/Card.jsx";
+import Filter from "./components/Filter";
+import Search from "./components/Search";
+import paises from "./data.js";
 
 function App() {
+  const paises12 = paises.slice(0, 12);
+  console.log(paises12);
+
   return (
     <>
-      <section className="element">Where in the world?</section>
+      <header>
+        <BarSup />
+        <section className="container-barra">
+          <div className="container-search-filter">
+            <Search />
+            <Filter />
+          </div>
+        </section>
+      </header>
+      <main>
+        <div className="container-barra">
+          <div className="cards">
+            {paises12?.map((pais) => (
+              <Card key={pais.name} pais={pais} />
+            ))}
+          </div>
+        </div>
+      </main>
     </>
   );
 }
