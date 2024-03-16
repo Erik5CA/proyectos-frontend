42e5623053be93d7780db1565857adb553245d8d
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-15 18:28:04 -0600

    se agrego funcionalidad para abrir el modal con informacion mas detallada

--- 02-rest-countries/src/components/Card.jsx
+++ 02-rest-countries/src/components/Card.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/prop-types */
 
-export default function Card({ pais }) {
+export default function Card({ pais, openModal, selection }) {
   const { name, population, flag, region, capital } = pais;
   return (
-    <div className="card">
-      <img className="card-image" src={flag} alt="" />
+    <div
+      className="card"
+      onClick={() => {
+        openModal();
+        selection(name);
+      }}
+    >
+      <img className="card-image" src={flag} alt="" loading="lazy" />
       <div className="card-titles">
         <h3 className="card-title">{name}</h3>
         <p>
