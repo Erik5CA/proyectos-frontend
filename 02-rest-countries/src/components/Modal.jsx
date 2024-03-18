003c6eae2cb67aec6fb683b86cd8bc9ef6680fca
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-18 15:36:41 -0600

    div para mostrar dos columnas en pantallas medianas y grandes

--- 02-rest-countries/src/components/Modal.jsx
+++ 02-rest-countries/src/components/Modal.jsx
@@ -11,43 +11,47 @@ export default function Modal({ onClose, country }) {
         </button>
         <div className="container-modal">
           <img src={country.flag} alt="" className="modal-image" />
-          <div className="modal-titles">
-            <h3 className="modal-title">{country.name}</h3>
-            <p>
-              <span>Native Name:</span> {country.nativeName}
-            </p>
-            <p>
-              <span>Population:</span> {country.population}
-            </p>
-            <p>
-              <span>Region:</span> {country.region}
-            </p>
-            <p>
-              <span>Sub Region:</span> {country.subregion}
-            </p>
-            <p>
-              <span>Capital:</span> {country.capital}
-            </p>
-          </div>
-          <div className="modal-titles">
-            <p>
-              <span>Top Level Domain:</span> {country.topLevelDomain}
-            </p>
-            <p>
-              <span>Currencies:</span> {country.currencies}
-            </p>
-            <p>
-              <span>Languages:</span> {country.languages}
-            </p>
-          </div>
           <div>
-            <h4>Borders:</h4>
-            <div className="container-bordes">
-              {country.borders?.map((borde) => (
-                <div key={borde} className="borde">
-                  {borde}
-                </div>
-              ))}
+            <h3 className="modal-title">{country.name}</h3>
+            <div className="container-info">
+              <div className="modal-titles">
+                <p>
+                  <span>Native Name:</span> {country.nativeName}
+                </p>
+                <p>
+                  <span>Population:</span> {country.population}
+                </p>
+                <p>
+                  <span>Region:</span> {country.region}
+                </p>
+                <p>
+                  <span>Sub Region:</span> {country.subregion}
+                </p>
+                <p>
+                  <span>Capital:</span> {country.capital}
+                </p>
+              </div>
+              <div className="modal-titles">
+                <p>
+                  <span>Top Level Domain:</span> {country.topLevelDomain}
+                </p>
+                <p>
+                  <span>Currencies:</span> {country.currencies}
+                </p>
+                <p>
+                  <span>Languages:</span> {country.languages}
+                </p>
+              </div>
+            </div>
+            <div>
+              <h4>Borders:</h4>
+              <div className="container-bordes">
+                {country.borders?.map((borde) => (
+                  <div key={borde} className="borde">
+                    {borde}
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
